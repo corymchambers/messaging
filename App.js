be643285f077c91f8ab1edf7659a5fd81f1c86f2
“Cory <corytyme@yahoo.com>
2020-02-11 08:17:02 -0500

    Added MessageList component, fixed spacing issues, and allowed to click on image for fullscreen effect

--- App.js
+++ App.js
@@ -1,57 +1,150 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, Alert, TouchableHighlight } from 'react-native';
 
 import Status from './components/Status'
+import MessageList from './components/MessageList'
+
+import { createImageMessage, createLocationMessage, createTextMessage } from './utils/MessageUtils'
 
 export default class App extends Component {
 
-    renderMessageList() {
-        return (
-            <View style={styles.content}></View>
-        )
-    }
+  state = {
+    messages: [
+      createImageMessage('https://unsplash.it/300/300'),
+      createTextMessage('World'),
+      createTextMessage('Hello'),
+      createLocationMessage({
+        latitude: 37.78825,
+        longitude: -122.4324
+      })
+    ],
+    fullscreenImageId: null
+  }
 
-    renderInputMethodEditor() {
-        return (
-            <View style={styles.inputMethodEditor}></View>
-        )
-    }
+  dismissFullscreenImage = () => {
+    this.setState({ fullscreenImageId: null })
+  }
 
-    renderToolbar() {
-        return (
-            <View style={styles.toolbar}></View>
+  handlePressMessage = ({ id, type }) => {
+    switch (type) {
+      case 'text':
+        Alert.alert(
+          'Delete message?',
+          'Are you sure you want to permanently delete this message?',
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel'
+            },
+            {
+              text: 'Delete',
+              style: 'desctructive',
+              onPress: () => {
+                const { messages } = this.state
+                this.setState({
+                  messages: messages.filter(
+                    message => message.id !== id
+                  )
+                })
+              }
+            }
+          ]
         )
+        break;
+      case 'image':
+        this.setState({ fullscreenImageId: id })
+        break;
+      default:
+        break;
     }
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Status />
-                {this.renderMessageList()}
-                {this.renderInputMethodEditor()}
-                {this.renderToolbar()}
-            </View>
-        )
-    }
+  renderMessageList() {
+    const { messages } = this.state
+
+    return (
+      <View style={styles.content}>
+        <MessageList
+          messages={messages}
+          onPressMessage={this.handlePressMessage}
+        />
+      </View>
+    )
+  }
+
+  renderInputMethodEditor() {
+    return (
+      <View style={styles.inputMethodEditor}></View>
+    )
+  }
+
+  renderToolbar() {
+    return (
+      <View style={styles.toolbar}></View>
+    )
+  }
+
+  renderFullscreenImage = () => {
+    const { messages, fullscreenImageId } = this.state
+    if (!fullscreenImageId) return null
+
+    const image = messages.find(
+      message => message.id === fullscreenImageId
+    )
+
+    if (!image) return null
+
+    const { uri } = image
+
+    return (
+      <TouchableHighlight
+        style={styles.fullscreenOverlay}
+        onPress={this.dismissFullscreenImage}
+      >
+        <Image style={styles.fullscreenImage} source={{ uri}} />
+      </TouchableHighlight>
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Status />
+        {this.renderMessageList()}
+        {this.renderInputMethodEditor()}
+        {this.renderToolbar()}
+        {this.renderFullscreenImage()}
+      </View>
+    )
+  }
 
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff'
-    },
-    content: {
-        flex: 1,
-        backgroundColor: 'white'
-    },
-    inputMethodEditor: {
-        flex: 1,
-        backgroundColor: 'white'
-    },
-    toolbar: {
-        borderTopWidth: 1,
-        borderTopColor: 'rgba(0,0,0,0.04)',
-        backgroundColor: 'white'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  content: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.04)',
+    backgroundColor: 'white'
+  },
+  fullscreenOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black',
+    zIndex: 2
+  },
+  fullscreenImage: {
+    flex: 1,
+    resizeMode: 'contain'
+  }
 });
--- components/Status.js
+++ components/Status.js
@@ -1,30 +1,84 @@
 import React, { Component } from 'react'
-import { StyleSheet, Platform, View, NetInfo, Text } from 'react-native'
+import { StyleSheet, Platform, View, Text, StatusBar } from 'react-native'
+import NetInfo from "@react-native-community/netinfo"
 import Constants from 'expo-constants'
 
 export default class Status extends Component {
-    state = {
-        isConnected: null
+  state = {
+    isConnected: null
+  }
+
+  async componentDidMount() {
+    // This is causing some issues, come back and fix later
+    const unsubscribe = NetInfo.addEventListener(state => {
+      this.setState({isConnected: state.isConnected})
+    });
+
+    // To unsubscribe to these update, just use:
+    unsubscribe();
+    NetInfo.fetch().then(state => {
+      this.setState({isConnected: state.isConnected})
+    });
+  }
+
+  render() {
+    const { isConnected } = this.state
+
+    const backgroundColor = isConnected ? 'white' : 'red'
+
+    const statusBar = (
+      <StatusBar
+        backgroundColor={backgroundColor}
+        barStyle={isConnected ? 'dark-content' : 'light-content'}
+        animated={false}
+      />
+    )
+
+    const messageContainer = (
+      <View style={styles.messageContainer} pointerEvents={'none'}>
+        {statusBar}
+        { !isConnected && (
+          <View style={styles.bubble}>
+            <Text>No network connection</Text>
+          </View>
+        )}
+     </View>
+    )
+
+    if (Platform.OS === 'ios') {
+      return (
+        <View style={[styles.status, {backgroundColor}]}>
+          {messageContainer}
+        </View>
+      )
     }
 
-    render() {
-        const { isConnected } = this.state
-
-        const backgroundColor = isConnected ? 'white' : 'red'
-        if (Platform.OS === 'ios') {
-            return (
-                <View style={[styles.status, {backgroundColor}]}></View>
-            )
-        }
-
-        return null
-    }
+    return null
+  }
 }
 const statusHeight = (Platform.OS === 'ios' ? Constants.statusBarHeight: 0)
 
 const styles = StyleSheet.create({
-    status: {
-        zIndex: 1,
-        height: statusHeight
-    }
+  status: {
+    zIndex: 1,
+    height: statusHeight
+  },
+  messageContainer: {
+    zIndex: 1,
+    position: 'absolute',
+    top: statusHeight + 20,
+    right: 0,
+    left: 0,
+    height: 80,
+    alignItems: 'center'
+  },
+  bubble: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    backgroundColor: 'red'
+  },
+  text: {
+    color: 'white'
+  }
 })
